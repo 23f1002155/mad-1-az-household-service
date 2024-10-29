002d2ce616757cd8fde6bcd5ce815d76dc97dd66
Aman Shrivastava <23f1002155@ds.study.iitm.ac.in>
2024-10-29 10:45:28 +0530

    Update of Work Till 29-10-2024

--- application/database.py
+++ application/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
-from app import app
+
 
 engine = None
 Base = declarative_base()
-db = SQLAlchemy(app)
+db = SQLAlchemy()
--- application/models.py
+++ application/models.py
@@ -6,9 +6,17 @@ class User(db.Model):
     u_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     u_name = db.Column(db.String, nullable = False, unique = True)
     u_email = db.Column(db.String, nullable = False, unique = True)
-    u_password = db.Column(db.String, nullable=False, unique=True)
-    u_role = db.Column(db.String, nullable=False)
+    u_passhash = db.Column(db.String(256), nullable=False, unique=True)
+    u_role = db.Column(db.Integer, nullable=False, foreign_key = True)
 
-'''class Sevice(db.Model):
-    __tablename__ = 'service database'
+class Role(db.Model):
+    __tablename__ = "user_role"
+    role_id = db.Column(db.Integer, primary_key = True)
+    role = db.Column(db.String, nullable = False, unique = True)
+
+    
+'''
+class Customer(db.Model)
+    __tablename__ = "customer_data"
+    c_id = db.Column(db.Integer, primary_key = True)
 '''
