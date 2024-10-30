05f746109e4cf3f46b52febebef51b0f492ffb8b
Aman Shrivastava <23f1002155@ds.study.iitm.ac.in>
2024-10-31 03:26:06 +0530

    Admin and Professional Login Completed on 31-10-2024

--- application/controllers.py
+++ application/controllers.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, flash, redirect, url_for
 from flask import render_template
 from flask import Blueprint
 from application.models import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
 my_blueprint = Blueprint('main', __name__)
 
@@ -13,11 +14,46 @@ def home():
 
 @my_blueprint.route("/login", methods = ["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            flash("All fields are required")
+            return redirect(url_for("login"))
+        
+
+        user = User.query.filter_by(u_name = username).first()
+        user_email = User.query.filter_by(u_email = username).first()
+
+        if user or user_email:
+            if user_email:
+                if check_password_hash(user_email.u_passhash, password) and user_email.u_role == 0:
+                    return redirect(url_for("main.admin_dashboard"))
+                elif check_password_hash(user_email.u_passhash, password) and user_email.u_role == 1:
+                    return redirect(url_for("customer_dashboard"))
+                elif check_password_hash(user_email.u_passhash, password) and user_email.u_role == 2:
+                    return redirect(url_for("service_provider_dashboard"))
+            elif user:
+                if check_password_hash(user.u_passhash, password) and user.u_role == 0:
+                    return redirect(url_for("main.admin_dashboard"))
+                elif check_password_hash(user.u_passhash, password) and user.u_role == 1:
+                    return redirect(url_for("customer_dashboard"))
+                elif check_password_hash(user.u_passhash, password) and user.u_role == 2:
+                    return redirect(url_for("service_provider_dashboard"))
+        else:
+            flash("Invalid credentials")
+            return redirect(url_for("login"))
+                    
+
 
 @my_blueprint.route("/sign-up-customer", methods = ["GET", "POST"])
 def sign_up_customer():
     return render_template("sign-up-customer.html")
+
+
 
 @my_blueprint.route("/sign-up-serviceprovider", methods = ["GET", "POST"])
 def sign_up_serviceprovider():
@@ -25,5 +61,41 @@ def sign_up_serviceprovider():
         services = Service.query.all()
         return render_template("sign-up-serviceprovider.html", services = services)
     elif request.method == "POST":
+        username = request.form.get("inputName")
+        email = request.form.get("inputEmail4")
+        password = request.form.get("inputPassword4")
+        confirm_password = request.form.get("inputConfirmPassword4")
+        contact_number = request.form.get("inputPhone")
+        service_id= request.form.get("inputService")
+        experience = request.form.get("inputExperience")
+        rate = request.form.get("inputRate")
+        service_description = request.form.get("inputServiceDescription")
+        address = request.form.get("inputAddress")
+        city = request.form.get("inputCity")
+        pincode = request.form.get("inputZip")
+
+        if not username or not email or not password or not confirm_password or not contact_number or not service_id or not experience or not rate or not service_description or not address or not city or not pincode:
+            flash("All fields are required")
+            return redirect(url_for("sign_up_serviceprovider"))
+        if password != confirm_password:    
+            flash("Passwords do not match")
+            return redirect(url_for("sign_up_serviceprovider"))
         
-        return render_template("sign-up-serviceprovider.html")
+        user = User.query.filter_by(u_email = email).first()
+        if user:
+            flash("Email already exists")
+            return redirect(url_for("sign_up_serviceprovider"))
+        
+        passhash = generate_password_hash(password)
+
+        new_user = User(u_name = username, u_email = email, u_passhash = passhash, u_role = 2)
+        new_provider = ServiceProvider(p_name = username, p_email = email, p_contact_number = contact_number, p_experience = experience, p_city = city, p_pincode = pincode, p_user_id = new_user.u_id, p_service_id = service_id)
+        db.session.add(new_user)
+        db.session.add(new_provider)
+        db.session.commit()
+        return redirect(url_for("login"))
+    
+
+@my_blueprint.route("/admin-dashboard")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
--- application/models.py
+++ application/models.py
@@ -1,21 +1,20 @@
 from .database import db
 
 
+class Role(db.Model):
+    __tablename__ = "user_role"
+    role_id = db.Column(db.Integer, primary_key = True)
+    role = db.Column(db.String, nullable = False, unique = True)
+
 class User(db.Model):
     __tablename__ = 'user_database'
     u_id = db.Column(db.Integer, autoincrement = True, primary_key = True)
     u_name = db.Column(db.String, nullable = False, unique = True)
     u_email = db.Column(db.String, nullable = False, unique = True)
     u_passhash = db.Column(db.String(256), nullable=False, unique=True)
-    u_role = db.Column(db.Integer, nullable=False, foreign_key = True)
+    u_role = db.Column(db.Integer, db.ForeignKey('user_role.role_id'))
 
-    #roles = db.relationship("Role", backref = "user_database", lazy = True)
-
-class Role(db.Model):
-    __tablename__ = "user_role"
-    role_id = db.Column(db.Integer, primary_key = True)
-    role = db.Column(db.String, nullable = False, unique = True)
-
+    roles = db.relationship("Role", backref = "user_database", lazy = True)
 
 
 
@@ -26,9 +25,9 @@ class Customer(db.Model):
     c_email = db.Column(db.String, nullable = False)
     c_city = db.Column(db.String, nullable = False)
     c_pincode = db.Column(db.Integer, nullable = False)
-    c_user_id = db.Column(db.Integer, foreign_key = True)
+    c_user_id = db.Column(db.Integer, db.ForeignKey('user_database.u_id'))
 
-    #user = db.relationship("User", backref = "customer_database", lazy = True)
+    user = db.relationship("User", backref = "customer_database", lazy = True)
 
 class ServiceProvider(db.Model):
     __tablename__ = "service_provider_database"
@@ -39,11 +38,11 @@ class ServiceProvider(db.Model):
     p_experience = db.Column(db.Integer, nullable = False)
     p_city = db.Column(db.String, nullable = False)
     p_pincode = db.Column(db.Integer, nullable = False)
-    p_user_id = db.Column(db.Integer, foreign_key = True)
-    p_service_id = db.Column(db.Integer, foreign_key = True)
+    p_user_id = db.Column(db.Integer, db.ForeignKey('user_database.u_id'))
+    p_service_id = db.Column(db.Integer, db.ForeignKey('service_database.s_id'))
 
-   # user = db.relationship("User", backref = "service_provider_database", lazy = True)
-    #service = db.relationship("Service", backref = "service_provider_database", lazy = True)
+    user = db.relationship("User", backref = "service_provider_database", lazy = True)
+    service = db.relationship("Service", backref = "service_provider_database", lazy = True)
 
 class Service(db.Model):
     __tablename__ = "service_database"
@@ -54,32 +53,32 @@ class Service(db.Model):
 class ServiceRequest(db.Model):
     __tablename__ = "service_request_database"
     sr_id = db.Column(db.Integer, primary_key = True)
-    sr_customer_id = db.Column(db.Integer, foreign_key = True)
-    sr_service_provider_id = db.Column(db.Integer, foreign_key = True)
-    sr_service_id = db.Column(db.Integer, foreign_key = True)
+    sr_customer_id = db.Column(db.Integer, db.ForeignKey('customer_database.c_id'))
+    sr_service_provider_id = db.Column(db.Integer, db.ForeignKey('service_provider_database.p_id'))
+    sr_service_id = db.Column(db.Integer, db.ForeignKey('service_database.s_id'))
     sr_status = db.Column(db.String, nullable = False)
     sr_date_time = db.Column(db.DateTime, nullable = False)
 
-   # customer = db.relationship("Customer", backref = "service_request_database", lazy = True)
-   # service_provider = db.relationship("ServiceProvider", backref = "service_request_database", lazy = True)
-   # service = db.relationship("Service", backref = "service_request_database", lazy = True)
+    customer = db.relationship("Customer", backref = "service_request_database", lazy = True)
+    service_provider = db.relationship("ServiceProvider", backref = "service_request_database", lazy = True)
+    service = db.relationship("Service", backref = "service_request_database", lazy = True)
 
 class ServiceFeedback(db.Model):
     __tablename__ = "service_feedback_database"
     sf_id = db.Column(db.Integer, primary_key = True)
-    sf_service_request_id = db.Column(db.Integer, foreign_key = True)
+    sf_service_request_id = db.Column(db.Integer, db.ForeignKey('service_request_database.sr_id'))
     sf_rating = db.Column(db.Integer, nullable = False)
     sf_feedback = db.Column(db.String, nullable = False)
 
-   # service_request = db.relationship("ServiceRequest", backref = "service_feedback_database", lazy = True)
+    service_request = db.relationship("ServiceRequest", backref = "service_feedback_database", lazy = True)
 
 class ServiceDetail(db.Model):
     __tablename__ = "service_detail_database"
     sd_id = db.Column(db.Integer, primary_key = True)
-    sd_service_id = db.Column(db.Integer, foreign_key = True)
-    sd_service_provider_id = db.Column(db.Integer, foreign_key = True)
+    sd_service_id = db.Column(db.Integer, db.ForeignKey('service_database.s_id'))
+    sd_service_provider_id = db.Column(db.Integer, db.ForeignKey('service_provider_database.p_id'))
     sd_service_rate = db.Column(db.Integer, nullable = False)
     sd_description = db.Column(db.String, nullable = False)
 
-   # service = db.relationship("Service", backref = "service_rate_database", lazy = True)
-   # service_provider = db.relationship("ServiceProvider", backref = "service_rate_database", lazy = True)
+    service = db.relationship("Service", backref = "service_rate_database", lazy = True)
+    service_provider = db.relationship("ServiceProvider", backref = "service_rate_database", lazy = True)
