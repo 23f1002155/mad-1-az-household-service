e7cce9703295c5696352a829e3d6511d5d4353b0
Aman Shrivastava <23f1002155@ds.study.iitm.ac.in>
2024-10-30 20:09:31 +0530

    Work Update Till 30-10-2024 with models added

--- application/models.py
+++ application/models.py
@@ -9,10 +9,13 @@ class User(db.Model):
     u_passhash = db.Column(db.String(256), nullable=False, unique=True)
     u_role = db.Column(db.Integer, nullable=False, foreign_key = True)
 
+    roles = db.relationship("Role", backref = "user_database", lazy = True)
+
 class Role(db.Model):
     __tablename__ = "user_role"
     role_id = db.Column(db.Integer, primary_key = True)
     role = db.Column(db.String, nullable = False, unique = True)
+
 
 
 
@@ -24,3 +27,49 @@ class Customer(db.Model):
     c_city = db.Column(db.String, nullable = False)
     c_pincode = db.Column(db.Integer, nullable = False)
     c_user_id = db.Column(db.Integer, foreign_key = True)
+
+    user = db.relationship("User", backref = "customer_database", lazy = True)
+
+class ServiceProvider(db.Model):
+    __tablename__ = "service_provider_database"
+    p_id = db.Column(db.Integer, primary_key = True)
+    p_name = db.Column(db.String, nullable = False)
+    p_email = db.Column(db.String, nullable = False)
+    p_contact_number = db.Column(db.Integer, nullable = False)
+    p_city = db.Column(db.String, nullable = False)
+    p_pincode = db.Column(db.Integer, nullable = False)
+    p_user_id = db.Column(db.Integer, foreign_key = True)
+    p_service_id = db.Column(db.Integer, foreign_key = True)
+
+    user = db.relationship("User", backref = "service_provider_database", lazy = True)
+    service = db.relationship("Service", backref = "service_provider_database", lazy = True)
+
+class Service(db.Model):
+    __tablename__ = "service_database"
+    s_id = db.Column(db.Integer, primary_key = True)
+    s_name = db.Column(db.String, nullable = False)
+    s_description = db.Column(db.String, nullable = False)
+    s_price = db.Column(db.Integer, nullable = False)
+
+class ServiceRequest(db.Model):
+    __tablename__ = "service_request_database"
+    sr_id = db.Column(db.Integer, primary_key = True)
+    sr_customer_id = db.Column(db.Integer, foreign_key = True)
+    sr_service_provider_id = db.Column(db.Integer, foreign_key = True)
+    sr_service_id = db.Column(db.Integer, foreign_key = True)
+    sr_status = db.Column(db.String, nullable = False)
+    sr_date_time = db.Column(db.DateTime, nullable = False)
+
+    customer = db.relationship("Customer", backref = "service_request_database", lazy = True)
+    service_provider = db.relationship("ServiceProvider", backref = "service_request_database", lazy = True)
+    service = db.relationship("Service", backref = "service_request_database", lazy = True)
+
+class ServiceFeedback(db.Model):
+    __tablename__ = "service_feedback_database"
+    sf_id = db.Column(db.Integer, primary_key = True)
+    sf_service_request_id = db.Column(db.Integer, foreign_key = True)
+    sf_rating = db.Column(db.Integer, nullable = False)
+    sf_feedback = db.Column(db.String, nullable = False)
+
+    service_request = db.relationship("ServiceRequest", backref = "service_feedback_database", lazy = True)
+
