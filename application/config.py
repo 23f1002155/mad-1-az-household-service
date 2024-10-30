a9389adb66ad3b704cafbfc318d1e0a123114cf7
Aman Shrivastava <23f1002155@ds.study.iitm.ac.in>
2024-10-30 19:24:52 +0530

    Work Update Till 30-10-2024 in config.py

--- application/config.py
+++ application/config.py
@@ -6,8 +6,10 @@ class Config():
     SQLITE_DB_DIR = None
     SQLALCHEMY_DATABASE_URI = None
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
 class LocalDevelopmentConfig(Config):
     SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
     SQLALCHEMY_DATABASE_URI = "sqlite:///"+ os.path.join(SQLITE_DB_DIR, "HomeHelpr.sqlite3")
     DEBUG = True
+    SECRET_KEY = os.getenv("SECRET_KEY")
